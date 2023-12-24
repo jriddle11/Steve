3ef7283cae00e7c01711ccf91b724d6f26d366c8
Josh Riddle <jriddle@ksu.edu>
2023-12-24 14:45:58 -0600

    note

--- src/Navbar.js
+++ src/Navbar.js
@@ -9,7 +9,13 @@ const Navbar = () => {
                 <Link to="/trump" className='steveButton' onClick={() => {window.scroll(0, 0); }}><span className='link triple'>2024</span></Link>
                 <Link to="/elon" className='steveButton' onClick={() => {window.scroll(0, 0); }}><span className='link triple'>Tesla</span></Link>
             </div>
-        </div>
+            <div className='notes'>
+                <h1>Steve:</h1><br/>
+                </div>
+                <div className='notes'>
+                <p> &emsp;&emsp;Thank you for always being such a badass guy to work with.<br/>I hope you have a merry Christmas and a happy new year. <br></br><br></br>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;- Josh</p>
+            </div>
+            </div>
      );
 }
  
